6c31e8026088c3cd68fb5455b099f934f4d5dbd3
Thomas Kunze <thomas.kunze@gmx.com>
2023-04-22 00:49:58 +0200

    don't change tokennotes on createhover

--- src/main/java/net/rptools/maptool/client/tool/PointerTool.java
+++ src/main/java/net/rptools/maptool/client/tool/PointerTool.java
@@ -2043,25 +2043,7 @@ public class PointerTool extends DefaultTool {
 
   private String createHoverNote(Token marker, boolean stubOnly) {
     var notes = StringUtil.htmlize(marker.getNotes(), marker.getNotesType());
-    if (!StringUtils.isEmpty(notes)) {
-      var text = notes;
-      text = text.replaceAll("white-space: pre-wrap", "");
-      text = text.replaceAll("white-space: pre", "");
-      text = text.replaceAll("size=\"[^\"]*\"", "");
-      marker.setNotes(text);
-      notes = marker.getNotes();
-    }
-
     var gmNotes = StringUtil.htmlize(marker.getGMNotes(), marker.getGmNotesType());
-    if (!StringUtils.isEmpty(gmNotes)) {
-      var text = gmNotes;
-      text = text.replaceAll("white-space: pre-wrap", "");
-      text = text.replaceAll("white-space: pre", "");
-      text = text.replaceAll("size=\"[^\"]*\"", "");
-      marker.setGMNotes(text);
-    }
-    gmNotes = marker.getGMNotes();
-
     boolean showGMNotes = !stubOnly && MapTool.getPlayer().isGM() && !StringUtil.isEmpty(gmNotes);
     boolean showNotes = !stubOnly && !StringUtil.isEmpty(notes);
 
