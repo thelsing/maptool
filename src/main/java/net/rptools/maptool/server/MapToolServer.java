a9946dcce00902222a82066b59ce6ce68205b261
Thomas Kunze <thomas.kunze@gmx.com>
2023-10-23 23:05:11 +0200

    no heartbeat on personal server

--- src/main/java/net/rptools/maptool/server/MapToolServer.java
+++ src/main/java/net/rptools/maptool/server/MapToolServer.java
@@ -76,7 +76,7 @@ public class MapToolServer {
     assetProducerThread.start();
 
     // Start a heartbeat if requested
-    if (!config.isPersonalServer()) {
+    if (config.isServerRegistered() && !config.isPersonalServer()) {
       heartbeatThread = new HeartbeatThread();
       heartbeatThread.start();
     }
